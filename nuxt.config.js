4b14d296c3534c49cd78c4b8ed377267b5378f24
denisk13005 <kirmanndenis@gmail.com>
2024-02-17 20:20:07 +0100

    t

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-
+    dir: 'dist'
   },
   generate: {
     dir: 'dist' // Remplacez 'monDossierDeSortie' par le nom de votre choix
